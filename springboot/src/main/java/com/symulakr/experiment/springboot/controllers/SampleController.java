6cb7fb357e037716de594bca9a7df1903de3a3f8
Eugene Derevyanko <ederevyanko@cybervisiontech.com>
2017-03-09 14:09:26 +0200

    added Spring Boot module

---
+++ springboot/src/main/java/com/symulakr/experiment/springboot/controllers/SampleController.java
@@ -0,0 +1,16 @@
+package com.symulakr.experiment.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+   @GetMapping("/")
+   @ResponseBody
+   public String get(){
+      return "Hello World!";
+   }
+
+}
